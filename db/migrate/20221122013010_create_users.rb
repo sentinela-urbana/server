0181c5b4d605d410d28ca1e313e57e78a88c94df
Marília Lorenzato <mariliaglorenzato@gmail.com>
2023-03-20 23:19:10 -0300

    improves of project

--- db/migrate/20221122013010_create_users.rb
+++ db/migrate/20221122013010_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.bigint :id
       t.string :taxpayer_registry
       t.datetime :birth_date
       t.string :address
--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_013010) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "user_types", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "taxpayer_registry"
     t.datetime "birth_date"
