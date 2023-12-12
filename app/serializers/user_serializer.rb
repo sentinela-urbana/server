a1a0367461ea3881f7e72eb93194ce6050026836
Marília Lorenzato <mariliaglorenzato@gmail.com>
2023-12-11 23:20:14 -0300

    feat: get user by id

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :full_name
+  attributes :id, :spot_id, :first_name, :last_name, :role
 
-  def full_name
-    "#{object.name} #{object.last_name}"
+  def spot_id
+    object.spot&.id
+  end
+
+  def first_name
+    object.name
+  end
+
+  def last_name
+    object.last_name
+  end
+
+  def role
+    object.role
   end
 end
