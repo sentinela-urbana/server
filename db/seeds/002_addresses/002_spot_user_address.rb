017837568a6b07c1499a144f9d89b43a2923457c
Marilia Lorenzato <mariliaglorenzato@gmail.com>
2023-12-13 20:18:29 -0300

    feat: add new spots

--- db/seeds/002_addresses/002_spot_user_address.rb
+++ db/seeds/002_addresses/002_spot_user_address.rb
@@ -44,3 +44,49 @@ spot.create_address(
   complement: Faker::Address.secondary_address,
   postal_code:
 )
+
+
+region = Region.first
+
+user = User.create(
+  name: 'Spot number 3',
+  last_name: 'Spot',
+  taxpayer_registry: '99914010027',
+  role: 'spot',
+  email: 'spot+3@email.com',
+  password: '123456',
+  password_confirmation: '123456'
+)
+
+postal_code = PostalCode.where(region:).first
+
+spot = Spot.create!(region:, user:)
+
+spot.create_address(
+  latitude: Faker::Address.latitude,
+  longitude: Faker::Address.longitude,
+  complement: Faker::Address.secondary_address,
+  postal_code:
+)
+
+
+user = User.create(
+  name: 'Spot number 4',
+  last_name: 'Spot',
+  taxpayer_registry: '99914010024',
+  role: 'spot',
+  email: 'spot+4@email.com',
+  password: '123456',
+  password_confirmation: '123456'
+)
+
+postal_code = PostalCode.where(region:).first
+
+spot = Spot.create!(region:, user:)
+
+spot.create_address(
+  latitude: Faker::Address.latitude,
+  longitude: Faker::Address.longitude,
+  complement: Faker::Address.secondary_address,
+  postal_code:
+)
